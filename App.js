1cda28e8884e14945fde8c86c75dc3942e5ba61f
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-09-30 21:04:06 +0300

    v0.4.2
    rename 'BottomTab' to 'BottomNavTab'
    
    add minor customization to BottomNavTab

--- App.js
+++ App.js
@@ -9,6 +9,8 @@
 import 'react-native-gesture-handler'; // to index.js?
 import React from 'react';
 
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import { NavigationContainer } from '@react-navigation/native';
 // import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
@@ -17,15 +19,34 @@ import MainScreen from 'screens/MainScreen';
 import BookmarksListScreen from 'screens/BookmarksListScreen';
 
 // const Stack = createStackNavigator();
-const BottomTab = createMaterialBottomTabNavigator();
+const BottomNavTab = createMaterialBottomTabNavigator();
 
 const App: () => React$Node = () => {
 	return (
 		<NavigationContainer>
-			<BottomTab.Navigator initialRouteName="MainScreen">
-				<BottomTab.Screen name="MainScreen" component={MainScreen} />
-				<BottomTab.Screen name="BookmarksList" component={BookmarksListScreen} />
-			</BottomTab.Navigator>
+			<BottomNavTab.Navigator initialRouteName="MainScreen"
+				activeColor="#09dbe3"
+				inactiveColor="gray"
+				barStyle={{ backgroundColor: 'pink' }}
+			>
+				<BottomNavTab.Screen name="MainScreen" component={MainScreen}
+					options={{
+						title: 'Home',
+						tabBarIcon: ({ color }) => (
+							<MaterialCommunityIcons name="home" color={color} size={26} />
+						)
+					}}
+				/>
+				<BottomNavTab.Screen name="BookmarksList" component={BookmarksListScreen}
+					options={{
+						title: 'Bookmarks',
+						
+						tabBarIcon: ({ color }) => (
+							<MaterialCommunityIcons name="bookmark-multiple" color={color} size={26} />
+						)
+					}}
+				/>
+			</BottomNavTab.Navigator>
 		</NavigationContainer>
 	);
 	// return (
