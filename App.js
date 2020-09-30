d6aa70fac43f1520014b8e9b1efe8c523a723424
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-09-30 20:39:22 +0300

    v0.4.1
    install:
      react-native-vector-icons (+ link it),
      @react-navigation/material-bottom-tabs,
      react-native-paper;
    
    add init BottomNavTab

--- App.js
+++ App.js
@@ -10,30 +10,40 @@ import 'react-native-gesture-handler'; // to index.js?
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 
 import MainScreen from 'screens/MainScreen';
 import BookmarksListScreen from 'screens/BookmarksListScreen';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+const BottomTab = createMaterialBottomTabNavigator();
 
 const App: () => React$Node = () => {
 	return (
 		<NavigationContainer>
-				<Stack.Navigator initialRouteName="MainScreen">
-					<Stack.Screen name="MainScreen" component={MainScreen}
-						options={{
-							title: 'All News'
-						}}
-					/>
-					<Stack.Screen name="BookmarksList" component={BookmarksListScreen}
-						options={{
-							title: 'Bookmarks'
-						}}
-					/>
-				</Stack.Navigator>
+			<BottomTab.Navigator initialRouteName="MainScreen">
+				<BottomTab.Screen name="MainScreen" component={MainScreen} />
+				<BottomTab.Screen name="BookmarksList" component={BookmarksListScreen} />
+			</BottomTab.Navigator>
 		</NavigationContainer>
 	);
+	// return (
+	// 	<NavigationContainer>
+	// 			<Stack.Navigator initialRouteName="MainScreen">
+	// 				<Stack.Screen name="MainScreen" component={MainScreen}
+	// 					options={{
+	// 						title: 'All News'
+	// 					}}
+	// 				/>
+	// 				<Stack.Screen name="BookmarksList" component={BookmarksListScreen}
+	// 					options={{
+	// 						title: 'Bookmarks'
+	// 					}}
+	// 				/>
+	// 			</Stack.Navigator>
+	// 	</NavigationContainer>
+	// );
 };
 
 export default App;
