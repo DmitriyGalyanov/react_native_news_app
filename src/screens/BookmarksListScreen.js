a2f92905931942936bf666a5b4fd4d347f62cb32
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-10-01 12:56:44 +0300

    v0.5.1
    edit BookmarksListScreen:
      add StackNavigator (for TopBar)

--- src/screens/BookmarksListScreen.js
+++ src/screens/BookmarksListScreen.js
@@ -4,13 +4,32 @@ import {View, Text} from 'react-native';
 
 import BookmarksList from 'components/BookmarksList';
 
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function BookmarksListScreen() {
+// import BookmarksListScreen from 'screens/BookmarksListScreen';
 
+
+const Stack = createStackNavigator();
+
+function BookmarksMainScreen() { // BookmarksMainScreen
 	return (
 		<View>
 			<Text>BookmarksListScreen</Text>
 			<BookmarksList />
 		</View>
+	)
+}
+
+
+export default function BookmarksListScreen() {
+
+	return (
+		<Stack.Navigator initialRouteName="BookmarksMainScreen">
+			<Stack.Screen name="BookmarksMainScreen" component={BookmarksMainScreen}
+				options={{
+					title: 'Bookmarks'
+				}}
+			/>
+		</Stack.Navigator>
 	)
 }
